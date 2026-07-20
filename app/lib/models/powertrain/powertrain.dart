f2ff695dc832342f4a78dd84bcc90a15c225d67b
jakej985-rgb <jakej985@gmail.com>
2026-07-20 04:23:46 -0600

    Update powertrain.dart

--- app/lib/models/powertrain/powertrain.dart
+++ app/lib/models/powertrain/powertrain.dart
@@ -1,1 +1,17 @@
-//todo
+import 'drivetrain.dart'
+
+class Drivetrain {
+  final String engineType;
+  final String engineSize;
+  final String engineCylinders;
+  final String transmissionType;
+  final drivetrain driveType;
+
+  Drivetrain({
+    required this.engineType,
+    required this.engineSize,
+    required this.engineCylinders,
+    required this.transmissionType,
+    required this.driveType,
+  });
+}
